e24e94de0512e2420d84fd845efb351875ce946e
Alec Smith <alecmsmith18@gmail.com>
2025-03-19 10:43:15 -0500

    Initial Commit

---
+++ SocialThread/SocialThreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialThreadApp.swift
+//  SocialThread
+//
+//  Created by Alec Smith on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SocialThreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
