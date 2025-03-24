b8bcb10a2644d2192c48cc517b11a8b69e8eb630
Alec Smith <alecmsmith18@gmail.com>
2025-03-23 20:43:19 -0500

    -- Create view model (ProfileModel) to structure data in seperate swift file -- Refactor code in project to conform to new view model -- Turn avatar image into reusable swiftui component -- Make View Model folder to store ProfileModel

---
+++ SocialThread/Components/ProfileImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileImageView.swift
+//  SocialThread
+//
+//  Created by Alec Smith on 3/23/25.
+//
+
+import SwiftUI
+
+struct ProfileImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileImageView()
+}
